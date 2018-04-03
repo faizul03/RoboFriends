f362121b2111dfcde6eec14b4e5ac6b1849713c5
Faizul Islam <35125068+faizul03@users.noreply.github.com>
2018-04-03 10:52:00 -0400

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,126 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Sam Smith',
+		username: 'Bird',
+		email: 'sam.Smith@wmail.com'
+	},
+
+	{
+		id: 2,
+		name: 'Sam Adam',
+		username: 'AustinPower',
+		email: 'samadam@gmail.com'
+	},
+
+	{
+
+		id: 3,
+		name: 'Peter Parker',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+
+		id: 4,
+		name: 'Sam William',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+
+		id: 5,
+		name: 'Mike Jone',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+
+		id: 6,
+		name: 'Herry Jorden',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+
+		id: 7,
+		name: 'Peter Parker',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+
+		id: 8,
+		name: 'Peter Parker',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+
+		id: 9,
+		name: 'Peter Parker',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+
+		id: 10,
+		name: 'Peter Parker',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+
+		id: 11,
+		name: 'Peter Parker',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+
+		id: 12,
+		name: 'Peter Parker',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+
+		id: 13,
+		name: 'Peter Parker',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+
+		id: 14,
+		name: 'Peter Parker',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+	{
+		id: 15,
+		name: 'Peter Parker',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+		
+	},
+	{
+		id: 16,
+		name: 'LoveS Parker',
+		username: 'Spidermen',
+		email: 'Parker@gmail.com'
+
+	},
+]
